89f834664a50570e490f79583704802c93486ae7
I-aM-FeNi <fenimabusa@gmail.com>
2023-03-29 15:28:32 -0700

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,25 +1,27 @@
 #include <stdio.h>
-/*
-* write your line of code here...
-* Remember:
-* - you are not allowed to use a
-* - you are not allowed to modify p
-* - only one statement
-* - you are not allowed to code anything else than this line of code
+/**
+* main - Entry point for program to demonstrate pointers and array indexing.
 *
-*(p + 5) = 98;
-* ...so that this prints 98\n
+* Initializes array `a` and sets `a[2]` to 1024. Sets `n` indirectly through
+* a pointer `p`, then prints `a[2]` (now 98).
+* return value of main indicates the success or failure of the program. In
+* this case, the program always returns 0 on success, indicating that it
+* executed without error.
+*
+* Returns 0 on success.
 */
 
 int main(void)
 {
-int n;
-int a[5];
-int *p;
+	int n;
+	int a[5];
+	int *p;
 
-a[2] = 1024;
-p = &n;
+	a[2] = 1024;
+	p = &n;
 
-printf("a[2] = %d\n", a[2]);
-return (0);
+	*(p + 2) = 98;
+
+	printf("a[2] = %d\n", a[2]);
+	return (0);
 }
