01c529ddf1d68f21b2a8092627c5829f722aa99a
I-aM-FeNi <fenimabusa@gmail.com>
2023-04-10 06:31:16 -0700

    C - Static libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _puts - prints a string to stdout
+* @str: pointer to string to be printed
+*/
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
